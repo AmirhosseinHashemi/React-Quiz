80caff40808febbc58708ff6b2d51a18e9181fb2
Amirhossein <4mirh03einhashemi@gmail.com>
2024-02-14 02:24:34 +0330

    🔨 fetching data from fake api and store it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 
+const initialState = {
+  questions: [],
+
+  // type of status : 👇
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+const reducer = function (state, action) {
+  switch (action.type) {
+    case "dataRecived":
+      return { ...state, questions: action.payLoad, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Unknown action type !");
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payLoad: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
